14b0afc4ba703cbbfaffdcdd545708d4f0d57414
iconicsudip <das366966@gmail.com>
2022-10-05 13:07:02 +0530

    procfile changed

--- sheetapi/settings.py
+++ sheetapi/settings.py
@@ -116,7 +116,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://sudip.ml",
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
