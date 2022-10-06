642faa3dc77deb7a24c4bdbfd7c5fdea03d21336
iconicsudip <das366966@gmail.com>
2022-10-06 20:20:59 +0530

    nan value deleted

--- api/views.py
+++ api/views.py
@@ -14,5 +14,18 @@ def sheetdata(request,code):
         for i in range(len(row)):
             temp_dic[row.keys()[i]] = row[i]
         dic[index] = temp_dic
-
-    return Response(dic)
+    count = 0
+    final_dic = dict()
+    for i in dic:
+        flag = False
+        for j in dic[i]:
+            try:
+                img = float(dic[i][j])
+                flag = True
+            except:
+                continue
+        if(flag==False):
+            print(dic[i])
+            final_dic[count] = dic[i]  
+            count+=1
+    return Response(final_dic)
