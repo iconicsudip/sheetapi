40a7d92bc5689841b3f173b2385447af7c378d83
iconicsudip <das366966@gmail.com>
2023-02-03 16:01:59 +0530

    vercel settings

--- sheetapi/settings.py
+++ sheetapi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y!(8t8s-yv7siqn21y@ws@i4+^r^%j@ffoitk_u_*oq-h@l2v3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -127,7 +127,7 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
